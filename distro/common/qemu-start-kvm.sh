ebf5013beff591bfe67f2463a69145c8e3f6f285
guanhe <307784732@qq.com>
2016-07-11 16:16:11 +0800

    lava-test-shell

--- distro/common/qemu-start-kvm.sh
+++ distro/common/qemu-start-kvm.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/expect -f
 spawn qemu-system-aarch64 -machine virt -cpu host -kernel Image_D02 -drive if=none,file=ubuntu.img,id=fs -device virtio-blk-device,drive=fs -append "console=ttyAMA0 root=/dev/vda1" -nographic -D -d -enable-kvm
-set timeout 40
+set timeout 20
 expect "estuary:/$"
-send "mount -t ext4 -o remount,rw /dev/vda1 /"
+send "mount -t ext4 -o remount,rw /dev/vda1 /\n"
 set timeout 5
-send "\n"
+expect "re-mounted"
 send \x01
-send "c"
-send "\n"
-send "quit"
-send "\n"
+send "c\n"
+send "quit\n"
 expect eof
-interact
-exit 0
+
