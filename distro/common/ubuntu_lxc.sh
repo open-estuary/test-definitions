a2f17ef7e726c6d9b36ad0272101c34c499d1007
wuyanjun <wuyanjun1027@163.com>
2016-07-21 19:41:12 +0800

    fix bug in smoke and common tests

--- distro/common/ubuntu_lxc.sh
+++ distro/common/ubuntu_lxc.sh
@@ -8,7 +8,7 @@ config_output=$(lxc-checkconfig)
 [[ $config_output =~ 'missing' ]] && print_info 1 lxc-checkconfig
 [[ $config_output =~ 'missing' ]] || print_info 0 lxc-checkconfig
 
-lxc-create -n $distro -t ubuntu-cloud -- -r vivid -T http://192.168.1.108:8083/docker-image/ubuntu-15.04-server-cloudimg-arm64-root.tar.gz
+lxc-create -n $distro -t ubuntu-cloud -- -r vivid -T http://htsat.vicp.cc:808/docker-image/ubuntu-15.04-server-cloudimg-arm64-root.tar.gz
 print_info $? lxc-create
 
 distro_exists=$(lxc-ls --fancy)
@@ -35,8 +35,8 @@ fi
 lxc-execute -n $distro /bin/echo hello
 print_info $? lxc-execute
 
-#lxc-attach -n $distro
-#print_info $? lxc-attach
+lxc-attach -n $distro
+print_info $? lxc-attach
 
 lxc-stop --name $distro
 print_info $? lxc-stop
@@ -45,13 +45,13 @@ lxc-destroy --name $distro
 print_info $? lxc-destory
 
 
-#$install_commands lxc-tests
-#install_results=$?
-#print_info $install_results install-lxc-tests
-#if [ $install_results -eq 0 ]; then
-#   for i in /usr/bin/lxc-test-*
-#   do 
-#       $i
-#       print_info $? "$i"
-#   done
-#fi
+$install_commands lxc-tests
+install_results=$?
+print_info $install_results install-lxc-tests
+if [ $install_results -eq 0 ]; then
+   for i in /usr/bin/lxc-test-*
+   do 
+       $i
+       print_info $? "$i"
+   done
+fi
--- distro/common/utils/sys_info.sh
+++ distro/common/utils/sys_info.sh
@@ -16,9 +16,9 @@ else
     distro="ubuntu"
 fi
 
-local_ip=$(ifconfig `route -n | grep "^0"|awk '{print $NF}'`|grep -o "addr inet:[0-9\.]*"|cut -d':' -f 2)
+local_ip=$(ip addr show `ip route | grep "default" | awk '{print $NF}'`| grep -o "inet [0-9\.]*" | cut -d" " -f 2)
 if [ ${local_ip}x = ""x ]; then
-    local_ip=$(ip addr show `ip route | grep "default" | awk '{print $NF}'`| grep -o "inet [0-9\.]*" | cut -d" " -f 2)
+    local_ip=$(ifconfig `route -n | grep "^0"|awk '{print $NF}'`|grep -o "addr inet:[0-9\.]*"|cut -d':' -f 2)
 fi
 
 restart_service='systemctl restart'
@@ -27,7 +27,7 @@ status_service='systemctl status'
 
 case $distro in 
     "ubuntu" | "debian" )
-        update_commands='apt-get update'
+        update_commands='apt-get update -y'
         install_commands='apt-get install -y'
         restart_service=""
         start_service=""
@@ -38,11 +38,11 @@ case $distro in
         install_commands='zypper -n install'
         ;;
     "centos" )
-        update_commands='yum update'
+        update_commands='yum update -y'
         install_commands='yum install -y'
         ;;
     "fedora" )
-        update_commands='dnf update'
+        update_commands='dnf update -y'
         install_commands='dnf install -y'
         ;;
 esac
