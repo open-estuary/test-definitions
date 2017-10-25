cf17e0e8e54b9c434effe7662cc0e50509be9fcf
victorChen201 <shuangshengchen@qq.com>
2017-10-25 15:58:30 +0800

    删除多余文件夹
    Signed-off-by: victorChen201 <shuangshengchen@qq.com>

--- app/hhvm/test_page/test_hhvm.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (defined('HHVM_VERSION')) {
-    echo 'HHVM is working';
-    #phpinfo();
-} else {
-    echo 'HHVM is not working';
-}
-?>
-
--- app/mysql/alisql_1/run_server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-./setup.sh server
-
-./scripts/init_server.sh init 1
-./scripts/init_server.sh start 1
