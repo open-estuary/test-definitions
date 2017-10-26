736a6cdaae5e879962287cee58539628a217d20a
root <root@localhost.localdomain>
2017-10-26 17:27:45 +0800

    add cockroach test case of insecure

--- app/cockroach/cockroachInsecureTest.sh
+++ app/cockroach/cockroachInsecureTest.sh
@@ -1,14 +1,16 @@
 #! /bin/bash 
 
-
+set -x
 basedir=$(cd `dirname $0` ;pwd)
 cd $basedir 
 . ../../lib/sh-test-lib 
+
 
 install_deps cockroach
 
 if [ `which cockroach`  ] ;then
     lava-test-case "cockroach install " --result pass 
+	echo "cockrock install ok"
 else
     lava-test-case "cockroach install" --result fail 
 fi
@@ -16,25 +18,38 @@ fi
 version=`cockroach version | grep "Build Tag:" | awk '{print $3}'`
 if [ $version = "v1.0.3"  ];then
     lava-test-case "cockroach version" --result pass 
+	echo "version ok"
 else
     lava-test-case "cockroach version" --result fail 
 fi
-
-cockroach start --insecure --host=localhsot --background
-
-if [ `ps -ef |grep cockroach | grep -v grep`  ];then
+if [ -d cockroach-data ];then
+    rm -rf cockroach-data
+fi
+cockroach start --insecure --host localhost --background   
+if [ `ps -ef | grep "cockroach start" | grep -v grep | wc -l` -eq 1 ];then
     lava-test-case "cockroach start node1" --result pass 
+	echo "cockroach start ok ----------------"
 else
     lava-test-case "cockroach start node1" --result fail 
 fi
+echo ----------------
 
+if [ -d node2 ];then
+    rm -rf node2
+fi
 cockroach start --insecure --store=node2 --host=localhost --port=26258 --http-port=8081 --join=localhost:26257 --background
+if [ -d node3 ];then
+    rm -rf node3
+fi
 cockroach start --insecure --store=node3 --host=localhost --port=26259 --http-port=8082 --join=localhost:26257 --background
 
-if [ `ps -ef |grep cockroach | grep -v grep | wc -l` = 3 ] ;then
+if [ `ps -ef |grep "cockroach start"| grep -v grep | wc -l` = 3 ] ;then
     lava-test-case "cockroach cluster start" --result pass 
+	echo "cockroach cluster start ok ------------------------"
 else
     lava-test-case "cockroach cluster start" --result fail
+    echo "cockroach cluster start failed"
+    exit 1
 fi
 echo 
 echo "cockroach insecure cluster start successed"
@@ -42,42 +57,62 @@ echo
 
 nodestatus1=`cockroach node ls --insecure` 
 nodestatus2=`cockroach node status --insecure`
-if [ $nodestatus1 != "" -a $nodestatus2 != ""  ] ;then
-    lava-test-case "cockroach status " --result pass 
+if `echo $nodestatus1 | grep "3 rows"` && `echo $nodestatus2 | grep "3 rows"`;then
+    lava-test-case "cockroach status " --result pass
+	echo "cockroach status ok ------------------------" 
 else
     lava-test-case "cockroach status" --result fail
 fi
 
+echo 
+echo "cockroach status ok"
+echo 
+
 cockroach sql --insecure -e "DROP DATABASE IF EXISTS bank;"
-res=`cockroach sql --insecure -e "CREATE TABLE bank.accounts (id INT PRIMARY , balance DECIMAL);
-                            INSERT INTO bank.accounts VALUES (1 , 1000.50);
-                            SELECT * FROM bank.accounts;"`
-if [ x$res = x"1 row id balance 1 1000.50" ] ;then
+res=`cockroach sql --insecure -e "CREATE DATABASE bank;
+				  CREATE TABLE bank.accounts (id INT PRIMARY KEY , balance DECIMAL);
+				  INSERT INTO bank.accounts VALUES (1 , 1000.50);
+				  SELECT * FROM bank.accounts;"`
+if [ `echo $res | grep "1 row" -c` -ge 1 ] ;then
     lava-test-case "cockroach node1 executer sql statement" --result pass 
 else
     lava-test-case "cockroach node1 executer sql statement" --result fail 
 fi
 
-node2res=`cockroach sql --insecure --port=26256 -e "SELECT * FROM bank.accounts"`
+node2res=`cockroach sql --insecure --port=26258 -e "SELECT * FROM bank.accounts"`
 
-if [ x$node2res = x"1 row id balance 1 1000.50" ] ;then
+if [  `echo $node2res | grep "1 row" -c` -ge 1  ] ;then
     lava-test-case "cockroach node2 executer sql statement" --result pass 
 else
     lava-test-case "cockroach node2 executer sql statement" --result fail 
 fi
 
-ps -ef | grep cockroach | grep node2 | grep -v grep | awk '{print $2}' | xargs kill -9
+#ps -ef | grep cockroach | grep node2 | grep -v grep | awk '{print $2}' | xargs kill -9
+cockroach quit --insecure --port 26258
 noderes=`cockroach sql --insecure -e "SELECT * FROM bank.accounts"`
 
-if [ x$noderes = x"1 row id balance 1 1000.50" ] ;then
+if [ `echo "$noderes"| grep "1 row" -c` -ge 1  ] ;then
     lava-test-case "cockroach single point failure" --result pass 
 else
     lava-test-case "cockroach single point failure" --result fail 
 fi
 
-ps -ef | grep cockroach | grep -v grep | awk '{print $2}'| xargs kill -9
-stopCluster=`ps -ef | grep cockroach | grep -v grep`
-if [ -z  $stopCluster ];then
+#ps -ef | grep cockroach | grep -v grep | awk '{print $2}'| xargs kill -9
+
+cockroach start --insecure --store=node2 --host=localhost --port=26258 --http-port=8081 --join=localhost:26257 --background
+
+if [ `ps -ef |grep "cockroach start" | grep -v grep | wc -l` -eq 3 ];then
+    lava-test-case "cockroach restart" --result pass
+else
+    lava-test-case "cockroach restart" --result fail
+fi
+
+cockroach quit --insecure --port 26257
+cockroach quit --insecure --port 26258
+cockroach quit --insecure --port 26259
+
+stopCluster=`ps -ef | grep "cockroach start" | grep -v grep`
+if [ -z  "$stopCluster" ];then
     lava-test-case "cockroach stop cluster" --result pass
 else
     lava-test-case "cockroach stop cluster" --result fail
@@ -92,4 +127,4 @@ else
 fi
 rm -rf cockroach-data node2 node3
 
-
+set +x
