49c89fa149fb9042aca75af6218c6bd04ade841c
AlamelaLiu <meili760628705@163.com>
2017-11-24 17:03:14 +0800

    add mysql-community case

---
+++ auto-test/mysql-community/testlike.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#select data from case table where author name ~= littlem
+
+EXPECT=$(which expect)
+$EXPECT << EOF | tee out.log
+set timeout 500
+spawn mysql -u root -p
+expect "password:"
+send "root\r"
+expect "mysql>"
+send "use test;\r"
+expect "Database changed"
+send "select * from case_tbl WHERE case_author LIKE 'littlem%';\r"
+expect "samba"
+send "exit\r"
+expect eof
+EOF
+
