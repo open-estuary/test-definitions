7da80e8b6b90848c97872217c15438aeec2e6e17
victorChen201 <shuangshengchen@qq.com>
2017-11-24 10:43:26 +0800

    修改kernel-pakacge

--- auto-test/kernel-package/kernel-package.sh
+++ auto-test/kernel-package/kernel-package.sh
@@ -32,11 +32,57 @@ case "${dist}" in
             fi
         done
         ;;
-    centos) 
-        package_list = "kernel-devel-4.12.0-estuary.1 kernel-headers-4.12.0-estuary.1 kernel-tools-4.12.0-estuary.1 kernel-tools-libs-4.12.0-estuary.1 kernel-tools-libs-devel-4.12.0-estuary.1 perf-4.12.0-estuary.1 python-perf-4.12.0-estuary.1"
+    centos)
+        version="4.12.0"
+        release="estuary.2"
+        from_repo="Estuary"
+        package_list = "kernel-devel kernel-headers kernel-tools kernel-tools-libs kernel-tools-libs-devel perf python-perf kernel-aarch64 kernel-debug kernel-debug-debuginfo kernel-debug-devel kernel-debuginfo kernel-debuginfo-common-aarch64 kernel-tools-debuginfo perf-debuginfo python-perf-debuginfo"
         for p in ${package_list};do
             echo "$p install"
             yum install -y $p
+            status=$?
+            if test $status -eq 0
+            then
+                echo "$p install  [PASS]" | tee -a ${RESULT_FILE}
+                from=$(yum info $p | grep "From repo" | awk '{print $4}')
+                if [ "$from" == "$from_repo" ];then
+                    echo "$p source is $from : PASS" | tee -a ${RESULT_FILE}
+                else
+                    echo "$p source is $from : FAILED" | tee -a ${RESULT_FILE}
+                fi
+
+                vs=$(yum info $p | grep Version | awk '{print $3}')
+                if [ "$vs" == "$version" ];then
+                    echo "$p version is $vs : PASS" | tee -a ${RESULT_FILE}
+                else
+                    echo "$p version is $vs : FAILED" | tee -a ${RESULT_FILE}
+                fi
+
+                rs=$(yum info $p | grep Release | awk '{print $3}')
+                if [ "$rs" == "$release" ];then
+                    echo "$p release is $rs : PASS" | tee -a ${RESULT_FILE}
+                else
+                    echo "$p release is $rs : FAILED" | tee -a ${RESULTFILE}
+                fi
+
+                yum remove -y $p
+                status=$?
+                if test $status -eq 0
+                then
+                    echo "$p remove  [PASS]" | tee -a ${RESULT_FILE}
+                else
+                    echo "$p remove [FAIL]"  | tee -a ${RESULT_FILE}
+                fi
+            else
+                echo "$p install [FAIL]"  | tee -a ${RESULT_FILE}
+            fi
+        done
+        ;;
+    ubuntu) 
+        package_list = "kernel-devel-4.12.0-estuary.1 kernel-headers-4.12.0-estuary.1 kernel-tools-4.12.0-estuary.1 kernel-tools-libs-4.12.0-estuary.1 kernel-tools-libs-devel-4.12.0-estuary.1 perf-4.12.0-estuary.1 python-perf-4.12.0-estuary.1"
+        for p in ${package_list};do
+            echo "$p install"
+            apt-get install -y $p
             status=$?
             if test $status -eq 0
             then
@@ -45,7 +91,7 @@ case "${dist}" in
                 echo "$p install [FAIL]"  | tee -a ${RESULT_FILE}
             fi
             echo "$p remove"
-            yum remove -y $p
+            apt-get remove -y $p
             status=$?
             if test $status -eq 0
             then
