1d2d0ecf37d08cb45c1cb9859449b86fb18d6be7
victorChen201 <shuangshengchen@qq.com>
2017-11-25 08:47:34 +0800

    新增ubuntu内核包安装卸载测试

--- auto-test/kernel-rpm/kernel-rpm.sh
+++ auto-test/kernel-rpm/kernel-rpm.sh
@@ -3,28 +3,49 @@
 . ../../lib/sh-test-lib
 OUTPUT="$(pwd)/output"
 RESULT_FILE="${OUTPUT}/result.txt"
+LOG="${OUTPUT}/log.txt"
 export RESULT_FILE
-kernel_name = "kernel-aarch64-4.12.0-estuary.1"
-dist_nam
+kernel_name= ""
+dist_name
 ! check_root && error_msg "This script must be run as root"
 create_out_dir "${OUTPUT}"
 case "${dist}" in
-    debian|ubuntu)
-        echo "not surpport"        
+    ubuntu)
+        kernel_name= "linux-image-4.12.0-501-generic"
+        sudo apt-get linux-dep ${kernel_name} | tee "${LOG}"
+        sudo apt-get source -b ${kernel_name} | tee "${LOG}"
+        status=$?
+        if test $status -eq 0
+        then
+            echo "rpmbuild [PASS]" | tee -a ${RESULT_FILE}
+        else
+            echo "rmpbuild [FAIL]" | tee -a ${RESULT_FILE}
+        fi
+        ;;
+    debian)
+        kernel_name= "linux-image-4.12.0-500-arm64"
+        apt-get linux-dev ${kernel_name} | tee "${LOG}"
+        status=$?
+        if test $status -eq 0
+        then
+            echo "rpmbuild [PASS]" | tee -a ${RESULT_FILE}
+        else
+            echo "rmpbuild [FAIL]" | tee -a ${RESULT_FILE}
+        fi
         ;;
     centos) 
+        kernel_name= "kernel-aarch64-4.12.0-estuary.2"
         yum install yum-utils
         yum-builddep -y kernel
         yum install -y pesign
         yumdownloader --source kernel
-        rpmbuild --rebuild  ${kernel_name} | tee  "${RESULT_FILE}"
+        rpmbuild --rebuild  ${kernel_name} | tee  "${LOG}"
         status=$?
         if test $status -eq 0
         then
-            echo "rpmbuild   [PASS]"
+            echo "rpmbuild  [PASS]" | tee -a "${RESULT_FILE}"
         else
-            echo "rmpbuild [FAIL]"
-            exit
+            echo "rmpbuild [FAIL]" | tee -a "${RESULT_FILE}"
         fi
         ;;
 esac
