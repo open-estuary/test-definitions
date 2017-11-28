975b580b9e78d0dc86c73117480336f6e52abe1d
lmm915815 <tanliqing2010@163.com>
2017-11-28 16:45:35 +0800

    add nodejs test case

---
+++ auto-test/nodejs/nodejs-test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#=================================================================
+#   文件名称：nodejs-test.sh
+#   创 建 者：tanliqing tanliqing2010@163.com
+#   创建日期：2017年11月28日
+#   描    述：
+#
+#================================================================*/
+
+
+basedir=$(cd `dirname $0`;pwd)
+cd $basedir
+. ../../lib/sh-test-lib
+. ../../utils/sys_info.sh
+
+source ./nodejs.sh 
+
+nodejs_install
+nodejs_npm
+nodejs_fs_test
+#nodejs_uninstall
