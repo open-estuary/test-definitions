00f0860b597a8dda5cc62440820f553884b87376
victorChen201 <shuangshengchen@qq.com>
2017-11-29 10:21:41 +0800

    完善内核包支持debian

--- auto-test/kernel-rpm/kernel-rpm.sh
+++ auto-test/kernel-rpm/kernel-rpm.sh
@@ -5,14 +5,15 @@ OUTPUT="$(pwd)/output"
 RESULT_FILE="${OUTPUT}/result.txt"
 LOG="${OUTPUT}/log.txt"
 export RESULT_FILE
-kernel_name= ""
+kernel_name=""
 dist_name
 ! check_root && error_msg "This script must be run as root"
 create_out_dir "${OUTPUT}"
+headers=$(uname -r)
 case "${dist}" in
     ubuntu)
-        kernel_name= "linux-image-4.12.0-501-generic"
-        sudo apt-get linux-dep ${kernel_name} | tee "${LOG}"
+        kernel_name="linux-image-${headers}"
+        sudo apt-get build-dep ${kernel_name} | tee "${LOG}"
         sudo apt-get source -b ${kernel_name} | tee "${LOG}"
         status=$?
         if test $status -eq 0
@@ -23,8 +24,9 @@ case "${dist}" in
         fi
         ;;
     debian)
-        kernel_name= "linux-image-4.12.0-500-arm64"
-        apt-get linux-dev ${kernel_name} | tee "${LOG}"
+        kernel_name="linux-image-${headers}"
+        apt-get build-dep ${kernel_name} | tee "${LOG}"
+        apt-get source -b ${kernel_name} | tee "${LOG}"
         status=$?
         if test $status -eq 0
         then
@@ -34,7 +36,7 @@ case "${dist}" in
         fi
         ;;
     centos) 
-        kernel_name= "kernel-aarch64-4.12.0-estuary.2"
+        kernel_name="kernel-aarch64-4.12.0-estuary.2"
         yum install yum-utils
         yum-builddep -y kernel
         yum install -y pesign
