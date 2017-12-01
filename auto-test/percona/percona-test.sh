04d1fc66ca043e308dbe4f4b0d5423bc7b8ec26b
lmm915815 <tanliqing2010@163.com>
2017-12-01 17:37:08 +0800

    add percona test case

---
+++ auto-test/percona/percona-test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#=================================================================
+#   文件名称：percona-test.sh
+#   创 建 者：tanliqing tanliqing2010@163.com
+#   创建日期：2017年11月30日
+#   描    述：
+#
+#================================================================*/
+
+
+basedir=$(cd `dirname $0`;pwd)
+cd $basedir
+. ../../lib/sh-test-lib
+. ../../utils/sys_info.sh
+
+source ./percona.sh 
+
+percona_install
+close_firewall_seLinux 
+percona_start 
+
+mysql_client
+mysql_create
+mysql_alter
+mysql_drop
+
+mysql_load_data 
+
+#percona_uninstall
