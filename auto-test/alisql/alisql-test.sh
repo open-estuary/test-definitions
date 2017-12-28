11c6dfa25457cd7b7174ef4b0f5f96e081453493
lmm915815 <tanliqing2010@163.com>
2017-12-28 18:38:54 +0800

    add alisql cassandra findbugs ycsb test case

---
+++ auto-test/alisql/alisql-test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#=================================================================
+#   文件名称：alisql-test.sh
+#   创 建 者：tanliqing tanliqing2010@163.com
+#   创建日期：2017年12月27日
+#   描    述：
+#
+#================================================================*/
+
+basedir=$(cd `dirname $0`;pwd)
+cd $basedir
+. ../../lib/sh-test-lib
+. ../../utils/sys_info.sh
+
+source ./alisql.sh 
+source ./../percona/mysql.sh 
+set -x
+export PS4='+{$LINENO:${FUNCNAME[0]}} '
+
+alisql_install
+alisql_start_custom
+
+mysql_client
+mysql_create
+mysql_alter
+mysql_drop
+mysql_load_data 
+mysql_select
+mysql_insert
+
+mysql_transaction 
+
+#mysql_log
+mysql_innodb
+
+alisql_stop_custom 
+
---
+++ auto-test/findbugs/findbugs-test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#=================================================================
+#   文件名称：findbugs-test.sh
+#   创 建 者：tanliqing tanliqing2010@163.com
+#   创建日期：2017年12月26日
+#   描    述：
+#
+#================================================================*/
+
+
+basedir=$(cd `dirname $0`;pwd)
+cd $basedir
+. ../../lib/sh-test-lib
+. ../../utils/sys_info.sh
+
+source ./findbugs.sh 
+ 
+set -x
+export PS4='+{$LINENO:${FUNCNAME[0]}} '
+
+install_findbugs
+findbugs_test
+uninstall_findbugs
