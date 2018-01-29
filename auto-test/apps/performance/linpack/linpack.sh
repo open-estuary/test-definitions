5ea3807030e1c5729d92d656eb3c78ec89448723
mahongxin <hongxin_228@163.com>
2018-01-29 18:08:18 +0800

    修改目录结构

---
+++ auto-test/apps/performance/linpack/linpack.sh
@@ -0,0 +1,56 @@
+#!/bin/sh -e
+
+set -x
+cd ../../../utils
+   . ./sh-test-lib
+   . ./sys_info.sh
+cd -
+ARRAY_SIZE="200"
+# Run Test.
+#detect_abi
+wget http://www.netlib.org/benchmark/linpackc.new
+print_info $? wget-linpackc
+
+mv linpackc.new linpack.c
+case $distro in
+    "centos")
+     yum install glibc-static -y
+     yum install gcc -y
+     print_info $? install-package
+     ;;
+   "ubuntu|debian")
+    apt-get install gcc -y
+    apt-get install buid-essential -y
+    apt-get install glibc-source -y
+    print_info $? install-package
+esac
+
+gcc -O3 -static -o linpack linpack.c -lm
+print_info $? gcc-linpack
+
+# shellcheck disable=SC2154
+( echo "${ARRAY_SIZE}"; echo "q" ) \
+  | ./linpack 2>&1 \
+  | tee -a linpack.log
+print_info $? run-linpack
+# Parse output.
+count=`ps -aux | grep linpack | wc -l`
+if [ $count -gt 0 ]; then
+    kill -9 $(pidof linpack)
+    print_info $? kill-linpack
+fi
+case $distro in
+    "centos")
+        yum remove glibc-static -y
+        yum remove gcc -y
+        print_info $? remove-package
+        ;;
+
+     "ubuntu")
+      apt-get remove gcc -y
+      apt-get remove glibc-source -y
+      apt-get remove buid-essential -y
+      print_info $? remove-package
+      ;;
+
+esac
---
+++ auto-test/distributions/distribution/network-basic/network-basic.sh
@@ -0,0 +1,72 @@
+#!/bin/sh
+#Author mahongxin <hongxin_228@163.com>
+set -x
+cd ../../../utils
+   . ./sys_info.sh
+   . ./sh-test-lib
+cd -
+# shellcheck disable=SC1091
+#. ../../lib/sh-test-lib
+#OUTPUT="$(pwd)/output"
+#RESULT_FILE="${OUTPUT}/result.txt"
+#export RESULT_FILE
+INTERFACE="eth0"
+
+#usage() {
+#    echo "Usage: $0 [-s <true|false>] [-i <interface>]" 1>&2
+#    exit 1
+#}
+
+#while getopts "s:i:" o; do
+#  case "$o" in
+ #   s) SKIP_INSTALL="${OPTARG}" ;;
+ #   i) INTERFACE="${OPTARG}" ;;
+  #  *) usage ;;
+  #esac
+#done
+
+install() {
+    pkgs="curl net-tools"
+    install_deps "${pkgs}" "${SKIP_INSTALL}"
+    print_info $? install-pkgs
+}
+
+run() {
+    test_case="$1"
+    test_case_id="$2"
+    echo
+    info_msg "Running ${test_case_id} test..."
+    info_msg "Running ${test_case} test..."
+    eval "${test_case}"
+    check_return "${test_case_id}"
+}
+
+# Test run.
+#create_out_dir "${OUTPUT}"
+
+install
+
+# Get default Route Gateway IP address of a given interface
+GATEWAY=$(ip route list  | grep default | awk '{print $3}')
+
+run "netstat -an" "print-network-statistics"
+print_info $? netstat
+run "ip addr" "list-all-network-interfaces"
+print_info $? ip-addr
+run "route" "print-routing-tables"
+print_info $? route
+run "ip link set lo up" "ip-link-loopback-up"
+print_info $? ip-link
+run "route" "route-dump-after-ip-link-loopback-up"
+print_info $? route-dump
+run "ip link set ${INTERFACE} up" "ip-link-interface-up"
+run "ip link set ${INTERFACE} down" "ip-link-interface-down"
+print_info $? ip-link
+run "dhclient -v ${INTERFACE}" "Dynamic-Host-Configuration-Protocol-Client-dhclient-v"
+print_info $? dhclient
+run "route" "print-routing-tables-after-dhclient-request"
+run "ping -c 5 ${GATEWAY}" "ping-gateway"
+print_info $? ping-gateway
+run "curl http://samplemedia.linaro.org/MPEG4/big_buck_bunny_720p_MPEG4_MP3_25fps_3300K.AVI -o curl_big_video.avi" "download-a-file"
+print_info $? curl
+remove_deps "${pkgs}"
