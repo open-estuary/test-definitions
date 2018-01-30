e122b818487a509ea73f8950dbea451c6f40af0d
lmm915815 <tanliqing2010@163.com>
2018-01-30 17:21:50 +0800

    update directory structure

---
+++ auto-test/middleware/mysql/alisql_1/run_server.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+./setup.sh server
+
+./scripts/init_server.sh init 1
+./scripts/init_server.sh start 1
---
+++ auto-test/middleware/spark/scala-test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#=================================================================
+#   文件名称：scala-test.sh
+#   创 建 者：tanliqing tanliqing2010@163.com
+#   创建日期：2018年01月10日
+#   描    述：
+#
+#================================================================*/
+
+
+basedir=$(cd `dirname $0`;pwd)
+cd $basedir
+. ../../lib/sh-test-lib
+. ../../utils/sys_info.sh
+
+source ./scala.sh 
+
+set -x
+
+export PS4='+{$LINENO:${FUNCNAME[0]}} '
+
+scala_install 
+scala_env_path 
+scala_test_if
+scala_test_for 
+scala_test_string
+scala_test_collection
+
