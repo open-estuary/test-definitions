fb13013c0797b27860d2db1d44c5687a889571bc
lmm915815 <tanliqing2010@163.com>
2018-02-05 16:50:19 +0800

    modify nginx test case

---
+++ auto-test/middleware/database/nginx/nginx-test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#=================================================================
+#   文件名称：nginx-test.sh
+#   创 建 者：tanliqing tanliqing2010@163.com
+#   创建日期：2018年02月05日
+#   描    述：
+#
+#================================================================*/
+
+
+
+
+basedir=$(cd `dirname $0`;pwd)
+cd $basedir
+. ../../lib/sh-test-lib
+. ../../utils/sys_info.sh
+
+source ./nginx.sh 
+set -x
+export PS4='+{$LINENO:${FUNCNAME[0]}} '
+
+nginx_install
+nginx_start 
+nginx_base_fun
+
+test_geoip_mod 
+
+nginx_stop 
+
+#nginx_remove
