0f9aee45a7cd3f4e4b8596e22f70808277f8819e
mahongxin <hongxin_228@163.com>
2018-04-02 17:53:58 +0800

    解压用例

--- auto-test/distributions/distribution/unzip/unzip.sh
+++ auto-test/distributions/distribution/unzip/unzip.sh
@@ -21,7 +21,14 @@ rm -f netperf-2.7.0.tar.gz
 tar -cvzf netperf-2.7.0.tar.gz netperf-2.7.0
 print_info $? tar-packaging
 
-wget http://htsat.vicp.cc:804/
-setenforce 0
-print_info $? open-selinux
+wget http://htsat.vicp.cc:804/cryptopp-CRYPTOPP_5_6_5.zip
+mv cryptopp-CRYPTOPP_5_6_5.zip cryp.zip
+unzip cryp.zip
+print_info $? unzip-compressedpackage
 
+rm -f cryp.zip
+zip cryp.zip cryptopp-CRYPTOPP_5_6_5
+print_info $? zip-packaging
+
+rm -rf netperf*
+rm -rf cryp*
