fc952538b04a2e3f1bad1fe4cdf2eeab70e385d2
yuhua-eric <15099996763@163.com>
2018-04-25 17:49:03 +0800

    add rv check

--- auto-test/kernel/linux/kernel-package2/kernel-package2.sh
+++ auto-test/kernel/linux/kernel-package2/kernel-package2.sh
@@ -105,12 +105,12 @@ case "${dist}" in
                       print_info 1 version
                 fi
 
-                # rs=$(yum info $p | grep "Release" | awk '{print $3}')
-                # if [ "$rs" = "$release" ];then
-                  #   print_info 0 release
-                # else
-                  #  print_info 1 release
-               # fi
+                rs=$(yum info $p | grep "Release" | awk '{print $3}')
+                if [ "$rs" = "$release" ];then
+                     print_info 0 release
+                else
+                     print_info 1 release
+                fi
                 #对于自带的包不去做卸载处理
                 # if test $rmflag -eq 0
                 # then
