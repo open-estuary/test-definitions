28c43fe25608cf4f2e9287f72b2b5ae2e75f3d18
yuhua-eric <15099996763@163.com>
2018-06-11 15:40:29 +0800

    fix ubuntu pro

--- auto-test/kernel/linux/kernel-package1/kernel-package1.sh
+++ auto-test/kernel/linux/kernel-package1/kernel-package1.sh
@@ -133,6 +133,7 @@ case "${dist}" in
         ;;
     ubuntu)
         sed -i s/5.[0-9]/5.1/g /etc/apt/sources.list.d/estuary.list 
+        apt-get update -q=2
         v='4.16.0-504'
         v1='4.16.0'
         package_list="linux-estuary linux-headers-estuary linux-source-estuary linux-tools-estuary linux-cloud-tools-common linux-doc linux-headers-${v} linux-headers-${v}-generic linux-image-${v}-generic linux-image-extra-${v}-generic linux-libc-dev linux-source-${v1} linux-tools-${v} linux-tools-${v}-generic linux-tools-common"
@@ -148,7 +149,7 @@ case "${dist}" in
             if test $status -eq 0
             then
                 print_info 0 install
-                from=$(apt show $p | grep "Source" | awk '{print $2}')
+                from=$(apt show $p | grep -w "Source" | awk '{print $2}')
                 if [ "$from" = "$from_repo1" -o "$from" = "$from_repo2" ];then
                 print_info 0 repo_check
                 else
