4c29bced8a39e5578b10d0a29b21b1bb47addb4c
dingyu1 <1136920311@qq.com>
2018-06-22 16:17:56 +0800

    hns10

---
+++ auto-test/peripheral/hns/hns-10.6/hns.10.6.sh
@@ -0,0 +1,44 @@
+#!bin/bash
+. ../../../utils/sh-test-lib
+. ../../../utils/sys_info.sh
+
+#GE网口详细统计数据获取容错测试
+inet=`ip link|grep 'e.*3'|awk '{print $2}'|sed 's/://g'`
+echo $inet
+res1=`ethtool -S $inet|grep "NIC statistics:"`
+if [ "$res1"x == "NIC statistics:"x ];then
+	print_info 0 ethtool_eth3
+else
+	print_info 1 ethtool_eth3
+fi
+
+#查询不存在设备名的统计数据
+inet1=`echo ${inet%?}`
+num=8
+inet2=${inet1}${num}
+ethtool -S $inet2 2> 6t1.txt
+result=`grep "No such device" 6t1.txt`
+if [ "$result"x != ""x ];then
+	print_info 0 ethtool_eth8
+else 
+	print_info 1 ethtool_eth8
+fi
+
+#查询空设备名的统计数据
+ethtool -S 2> 6t2.txt
+result1=`grep "bad command line" 6t2.txt`
+if [ "result1"x != ""x ];then
+	print_info 0 ethtool_none
+else
+	print_info 1 ethtool_none
+fi
+
+
+
+
+
+
+
+
+
+
