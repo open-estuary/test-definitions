a648e9ed6e8a7113a339f5b4d02e9e83b86428e7
xuexing1 <xuexing930@126.com>
2018-07-09 17:47:29 +0800

    add dir

---
+++ auto-test/peripheral/hns/hns13.2/hns13.2.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+cd ../../../../utils
+    . ./sys_info.sh
+    . ./sh-test-lib
+cd -
+case $distro in
+    "centos"|"fedora"|"opensuse")
+	#yum install net-tools -y
+	pkgs="net-tools"
+	install_deps "$pkgs"
+        ;;
+        ubuntu|debian)
+        #apt-get install net-tools -y
+	pkgs="net-tools"
+        install_deps "$pkgs" 
+esac
+	
+inet=`ip link|grep "state UP"|awk '{print $2}'|head -1|sed 's/0:/9/g'`
+echo $inet
+
+ETG=`ethtool -g $inet`
+echo $ETG
+
+if [ '$ETG|grep "No such device"' != "" ]; then
+	lava-test-case "fault tolerance test" --result pass
+else
+	lava-test-case "fault tolerance test" --result fail
+fi
+
+
