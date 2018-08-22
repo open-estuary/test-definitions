61e80b06387844439fcee4fa55a2b2f392a78e9c
dingyu1 <1136920311@qq.com>
2018-08-22 16:11:43 +0800

    modify openssh

--- auto-test/distributions/distribution/openssh/openssh.sh
+++ auto-test/distributions/distribution/openssh/openssh.sh
@@ -6,23 +6,26 @@ cd ../../../../utils
     . ./sys_info.sh
     . ./sh-test-lib
 cd -
-#distro=`cat /etc/redhat-release | cut -b 1-6`
+
+if [ `whoami` != "root" ];then
+	echo "YOu must be the root to run this script" >$2
+	exit 1
+fi
+
 case $distro in
-    "ubuntu")
-        #apt-get install openssh-server  -y
-        #apt-get install expect -y
+    "ubuntu"|"debian")
         pkgs="openssh-server expect"
         install_deps "${pkgs}"
         print_info $? install-package
         ;;
-    "centos")
-        yum install openssh-server.aarch64 -y
-        yum install expect -y
-        print_info $? install-package
+    "centos"|"fedora")
+        pkgs="openssh-server.aarch64 expect"
+        install_deps "${pkgs}"
+	print_info $? install-package
         ;;
     "opensuse")
-        zypper install -y openssh
-        zypper install -y expect
+	pkgs="openssh expect"
+	install_deps "${pkgs}"
         print_info $? install-package
         ;;
 esac
@@ -34,22 +37,22 @@ FTP_GET_LOG=sftp_get_test.log
 mkdir tmp && cd tmp
 echo 'For ftp put testing' > $FTP_PUT_LOG
 echo 'For ftp get testing' > ~/$FTP_GET_LOG
+
 #测试ssh登录是否成功
 EXPECT=$(which expect)
 $EXPECT << EOF
 set timeout 100
 spawn ssh localhost
-#expect "Are you sure"
 expect {
-"*yes/no" { send "yes\r"; exp_continue }
-"*assword:" { send "root\r" }
-}
-#send "yes\n"
-#expect "*assword:"
-#send "root\n"
+"*yes/no" 
+{send "yes\r";exp_continue;}
+"*assword:" 
+{send "root\r";}
+} 
 expect eof
 EOF
 print_info $? test-login
+
 #for get and put test "EXPECT=$(which expect)
 $EXPECT << EOF
 set timeout 100
@@ -64,29 +67,24 @@ expect "sftp>"
 send "quit\r"
 expect eof
 EOF
+
 if [ $(find . -maxdepth 1 -name "$FTP_GET_LOG")x != ""x ]; then
-    lava-test-case sftp-download --result pass
+	print_info 0  sftp-get
 else
-    lava-test-case sftp-download --result fail
+	print_info 1  sftp-get
 fi
-cd -
+
+cd ../
+rm -rf tmp
 
 cd ~
 
 if [ $(find . -maxdepth 1 -name "sftp_put_test.log")x != ""x ]; then
-    lava-test-case sftp-upload --result pass
+	print_info 0  sftp-put
 else
-    lava-test-case sftp-upload --result fail
+	print_info 0  sftp-put
 fi
 
-rm -rf tmp
-case $distro in
-    "ubuntu")
-        apt-get remove expect -y
-        print_info $? remove-package
-        ;;
-    "centos")
-        yum remove expect -y
-        print_info $? remove-package
-        ;;
-esac
+
+remove_deps "expect"
+print_info $? remove-package
