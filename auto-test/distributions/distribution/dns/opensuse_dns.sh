61379350ef69bfbe7b6d55f1295db1baaf475a09
liubeijie <m15177445676>
2018-08-24 11:05:14 +0800

    dns

--- auto-test/distributions/distribution/dns/opensuse_dns.sh
+++ auto-test/distributions/distribution/dns/opensuse_dns.sh
@@ -12,10 +12,11 @@ case $distro in
     "opensuse")
         zypper install -y bind ;
         zypper install -y expect;
+	print_info $? install-dns
         ;;
 esac
 mv  /etc/named.conf /etc/named.conf_bak
-cat << EOF >> /etc/named.conf
+cat << EOF> /etc/named.conf
 zone "tonv.my" in {
     type master;
     file "/var/lib/named/master/tonv.my.db";
@@ -27,7 +28,7 @@ zone "1.168.192.in-addr.arpa" in {
 EOF
 cd /var/lib/named/master
 #mkdir zones
-cat << EOF >> /var/lib/named/master/tonv.my.db
+cat << EOF > /var/lib/named/master/tonv.my.db
 tonv.my. IN SOA ns1.tonv.my. admin.tonv.my. (
 2007031001
 28800
@@ -40,7 +41,7 @@ www IN A 192.168.1.70
 ns1 IN A 192.168.1.80
 mail IN A 192.168.1.90
 EOF
-cat << EOF >> /var/lib/named/master/rev.1.168.192.in-addr.arpa
+cat << EOF > /var/lib/named/master/rev.1.168.192.in-addr.arpa
 @ IN SOA ns1.tonv.my. admin.tonv.my. (
 2007031001;
 28800;
@@ -54,16 +55,27 @@ cat << EOF >> /var/lib/named/master/rev.1.168.192.in-addr.arpa
 90 IN PTR mail.tonv.my.
 EOF
 systemctl restart named.service
-board_ip=`ifconfig |grep "inet"|cut -c21-34|head -n 1`
-sed -i "1i nameserver ${board_ip}" /etc/resolv.conf
+print_info $? start-dns
+#board_ip=`ifconfig |grep "inet"|cut -c14-26|head -n 1`
+board_ip=`ip a |grep "inet"|cut -c10-18|head -n 1`
+sed -i "s/nameserver/#nameserver/g" /etc/resolv.conf
+sed -i "nameserver ${board_ip}" /etc/resolv.conf
 systemctl restart named.service
 throu=`host www.tonv.my|grep -Po "has address"`
-
+print_info $? forward-dns
 #throu = `grep -Po "192.168.1.70" nfs.log`
-TCID="opensuse_dns test"
-if [ "$throu" != "" ]; then
-    lava-test-case $TCID --result pass
-else
-    lava-test-case $TCID --result fail
-fi
+case $distro in
+     "opensuse")
+	 zypper remove -y bind ;
+ 	 zypper remove -y expect;
+         print_info $? remove-dns
+         ;;
+
+esac
+#TCID="opensuse_dns test"
+#if [ "$throu" != "" ]; then
+#    lava-test-case $TCID --result pass
+#else
+#    lava-test-case $TCID --result fail
+#fi
 
