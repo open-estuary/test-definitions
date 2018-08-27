19a87a5e5f26680aa3e5d5651a12512693452b48
liubeijie <m15177445676>
2018-08-27 15:50:34 +0800

    devtoolset-4-yaml-cpp

--- auto-test/middleware/tool/devtoolset-4-yaml-cpp/devtoolset-4-yaml-cpp.sh
+++ auto-test/middleware/tool/devtoolset-4-yaml-cpp/devtoolset-4-yaml-cpp.sh
@@ -1,9 +1,8 @@
-
 #!/bin/sh 
 set -x
 cd ../../../../utils
-    .        ./sys_info.sh
-             ./sh-test-lib
+. ./sys_info.sh
+. ./sh-test-lib
 cd -
 
 if [ `whoami` != 'root' ] ; then
@@ -13,27 +12,29 @@ fi
 
 version="0.5.3"
 from_repo="Estuary"
-package="devtoolset-4-yaml-cpp"
+#package="devtoolset-4-yaml-cpp"
 
-for P in ${package};do
-    echo "$P install"
+#for P in ${package};do
+ #   echo "$P install"
 case $distro in
     "centos" )
-         yum install -y $P 
-         print_info $? devtoolset-4-yaml-cpp
-         ;;
- esac
-
+        package="devtoolset-4-yaml-cpp"
+        install_deps "${package}" 
+        print_info $? devtoolset-4-yaml-cpp
+        ;;
+esac
 # Check the package version && source
-from=$(yum info $P | grep "From repo" | awk '{print $4}')
+case $distro in
+      "centos")
+from=$(yum info $package | grep "From repo" | awk '{print $4}')
 if [ "$from" = "$from_repo"  ];then
       print_info 0 repo_check
 else
     rmflag=1
     if [ "$from" != "Estuary"  ];then
-        yum remove -y $P
-        yum install -y $P
-        from=$(yum info $P | grep "From repo" | awk '{print $4}')
+        yum remove -y $package
+        yum install -y $package
+        from=$(yum info $package | grep "From repo" | awk '{print $4}')
         if [ "$from" = "$from_repo"   ];then
               print_info 0 repo_check
         else
@@ -42,14 +43,15 @@ else
     fi
 fi
 
-vers=$(yum info $P | grep "Version" | awk '{print $3}')
+vers=$(yum info $package | grep "Version" | awk '{print $3}')
 if [ "$vers" = "$version"   ];then
       print_info 0 version
 else
      print_info 1 version
 fi
-done
+;;
+esac
 
 # Remove package
-yum remove -y $P
+yum remove -y $package
 print_info $? remove
