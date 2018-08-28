baabdd2bcec7977f8703d54c703d84477ffbd8b7
liubeijie <m15177445676>
2018-08-28 14:17:36 +0800

    liblsan

--- auto-test/middleware/tool/liblsan/liblsan.sh
+++ auto-test/middleware/tool/liblsan/liblsan.sh
@@ -1,9 +1,8 @@
-
 #!/bin/sh 
 set -x
 cd ../../../../utils
-    .        ./sys_info.sh
-             ./sh-test-lib
+.  ./sys_info.sh
+.  ./sh-test-lib
 cd -
 
 if [ `whoami` != 'root' ] ; then
@@ -13,27 +12,39 @@ fi
 
 version="6.2.1"
 from_repo="Estuary"
-package="liblsan"
-for P in ${package};do
-    echo "$P install"
+#package="liblsan"
+#for P in ${package};do
+#    echo "$P install"
 
 case $distro in
     "centos" )
-         yum install -y $P
-         print_info $? $P
+         package="liblsan"
+         install_deps "${package}"
+         print_info $? install-package
          ;;
+     "ubuntu"|"opensuse")
+	 package="liblsan0"
+	 install_deps "${package}"
+	 print_info $? install-package
+         ;;
+      "fedora")
+	  package="liblsan.aarch64"
+	  install_deps "${package}"
+	  print_info $? install-package
+	  ;;
  esac
-
+case $distro in
+     "centos")
 # Check the package version && source
-from=$(yum info $P | grep "From repo" | awk '{print $4}')
+from=$(yum info $package | grep "From repo" | awk '{print $4}')
 if [ "$from" = "$from_repo"  ];then
     print_info 0 repo_check
 else
    rmflag=1
    if [ "$from" != "Estuary"  ];then
-   yum remove -y $P
-   yum install -y $P
-   from=$(yum info $P | grep "From repo" | awk '{print $4}')
+   yum remove -y $package
+   yum install -y $package
+   from=$(yum info $package | grep "From repo" | awk '{print $4}')
       if [ "$from" = "$from_repo"   ];then
             print_info 0 repo_check
       else
@@ -42,14 +53,61 @@ else
     fi
 fi
 
-vers=$(yum info $P | grep "Version" | awk '{print $3}')
+vers=$(yum info $package | grep "Version" | awk '{print $3}')
 if [ "$vers" = "$version"   ];then
      print_info 0 version
 else
     print_info 1 version
 fi
-done
 
-# Remove package
-yum remove -y $P
-print_info $? remove
+        ;;
+esac
+case $distro in
+     "ubuntu")
+      from=$(apt show $package|grep Source|head -1|awk '{print $2}')
+      print_info $? $from
+      vers=$(apt show $package|grep Version|awk '{print $2}')
+      print_info $? $vers
+	;;
+esac
+case $distro in
+     "opensuse")
+	     from=$(zypper info $package | grep "Repo" | awk '{print $3}')
+	     if [ "$from" = "$from_repo"  ];then
+		         print_info 0 repo_check
+		 else
+	     rmflag=1
+	     if [ "$from" != "Estuary"  ];then
+	     zypper remove -y $package
+	     zypper install -y $package
+	     from=$(zypper info $package | grep "From repo" | awk '{print $4}')
+	     if [ "$from" = "$from_repo"   ];then
+	        print_info 0 repo_check
+	     else
+		print_info 1 repo_check
+		fi													      					
+             fi
+     fi
+      vers=$(zypper info $package|grep Version|awk '{print $3}')
+      print_info $? $vers
+	 ;;
+    	     
+esac
+case $distro in
+     "fedora")
+	 from=$(yum info $package|grep "From repo"|awk '{print $4}')
+	 print_info $? $from
+	 vers=$(yum info $package|grep Version|awk '{print $3}')
+	 print_info $? $vers
+	     ;;
+esac
+
+#done
+case $distro in 
+    "centos"|"ubuntu"|"fedora"|"opensuse")
+       remove_deps "${package}"
+       print_info $? remove_package
+       ;;
+esac
+
+
