8341589c6fb81be60475c15252b77e478862f2b7
wangsisi0 <zsxywss@sina.com>
2018-08-30 19:14:52 +0800

    modify

--- auto-test/kernel/linux/kernel-package1/kernel-package1.sh
+++ auto-test/kernel/linux/kernel-package1/kernel-package1.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+﻿#!/bin/bash
 
 cd ../../../../utils
     .        ./sys_info.sh
@@ -13,13 +13,15 @@ dist_name
 create_out_dir "${OUTPUT}"
 case "${dist}" in
     debian)
+        sed -i s/5.[0-9]/5.1/g /etc/apt/sources.list.d/estuary.list
+        apt-get update
         from_repo1='linux-estuary-latest'
         from_repo2='linux'
-        v='1'
-        version1="4.12+50${v}"
-        version2="4.12.0.estuary.50${v}-1"
-        version3="2.0+4.12.0.estuary.50${v}-1"
-        package_list="libcpupower1 libcpupower-dev libusbip-dev linux-cpupower linux-estuary-doc linux-estuary-perf linux-estuary-source linux-headers-4.12.0-50${v}-all linux-headers-4.12.0-50${v}-all-arm64 linux-headers-4.12.0-50${v}-arm64 linux-headers-4.12.0-50${v}-common linux-headers-estuary-arm64 linux-image-4.12.0-50${v}-arm64 linux-image-4.12.0-50${v}-arm64-dbg linux-image-estuary-arm64 linux-image-estuary-arm64-dbg linux-kbuild-4.12 linux-libc-dev linux-perf-4.12 linux-source-4.12 linux-support-4.12.0-50${v} usbip"
+        v='2'
+        version1="4.16+${v}"
+        version2="4.16.0.estuary.${v}-1"
+        version3="2.0+4.16.0.estuary.${v}-1"
+        package_list="libcpupower1 libcpupower-dev  linux-cpupower linux-estuary-doc linux-estuary-perf linux-estuary-source linux-headers-4.16.0-${v}-all linux-headers-4.16.0-${v}-all-arm64 linux-headers-4.16.0-${v}-arm64 linux-headers-4.16.0-${v}-common linux-headers-estuary-arm64 linux-image-4.16.0-${v}-arm64 linux-image-estuary-arm64 linux-kbuild-4.16 linux-libc-dev linux-perf-4.16 linux-source-4.16 linux-support-4.16.0-${v} usbip"
         for p in ${package_list};do
             echo "$p install"
             apt-get install -y $p
@@ -148,7 +150,7 @@ case "${dist}" in
             rmflag=0
             if test $status -eq 0
             then
-                print_info 0 install
+                print_info 0 installa
                 from=$(apt show $p | grep -w "Source" | awk '{print $2}')
                 if [ "$from" = "$from_repo1" -o "$from" = "$from_repo2" ];then
                 print_info 0 repo_check
@@ -175,6 +177,71 @@ case "${dist}" in
                 else
                     print_info 1 remove
                 fi
+            fi
+        done
+        ;;
+    fedora)
+        sed -i s/5.[0-9]/5.1/g /etc/yum.repos.d/estuary.repo
+        yum update
+        version="4.16.0"
+        release="estuary.6"
+        from_repo="Estuary"
+        package_list="kernel-devel kernel-headers kernel-tools-libs kernel-tools-libs-devel perf python-perf  kernel-debug kernel-debug-debuginfo"
+        for p in ${package_list};do
+            echo "$p install"
+            yum install -y $p
+            status=$?
+            rmflag=0
+            if test $status -eq 0
+            then
+                 print_info 0 install
+                from=$(yum info $p | grep "From repo" | awk '{print $4}')
+                if [ "$from" = "$from_repo" ];then
+                   print_info 0 repo_check
+                else
+                    #已经安装，但是安装源不是estuary的情况需要卸载重新安装
+                    rmflag=1
+                    if [ "$from" != "Estuary" ];then
+                        yum remove -y $p
+                        yum install -y $p
+                        from=$(yum info $p | grep "From repo" | awk '{print $4}')
+                        if [ "$from" = "$from_repo" ];then
+                             print_info 0 repo_check
+                        else
+                            print_info 1 repo_check
+                        fi
+                    fi
+                fi
+
+                vs=$(yum info $p | grep "Version" | awk '{print $3}')
+                if [ "$vs" = "$version" ];then
+                      print_info 0 version
+                else
+                      print_info 1 version
+                fi
+
+                rs=$(yum info $p | grep "Release" | awk '{print $3}')
+                if [ "$rs" = "$release" ];then
+                     print_info 0 release
+                else
+                     print_info 1 release
+                fi
+                #对于自带的包不去做卸载处理
+                if test $rmflag -eq 0
+                then
+                    yum remove -y $p
+                    status=$?
+                    if test $status -eq 0
+                    then
+                        print_info 0 remove
+                    else
+                        print_info 1 remove
+                    fi
+                else
+                    echo "$p don't remove" | tee -a ${RESULT_FILE}
+                fi
+            else
+                echo "$p install [FAIL]"  | tee -a ${RESULT_FILE}
             fi
         done
         ;;
