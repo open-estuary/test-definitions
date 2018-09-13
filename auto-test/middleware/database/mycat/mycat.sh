79f5ab41efb5f2b98dfbf9ee55649724464fcf86
mahongxin <hongxin_228@163.com>
2018-09-13 17:29:29 +0800

    mycat

--- auto-test/middleware/database/mycat/mycat.sh
+++ auto-test/middleware/database/mycat/mycat.sh
@@ -1,39 +1,71 @@
-#!/bin/sh
+#!/bin/bash
 # Copyright (C) 2017-8-29, Linaro Limited.
 #qperf is a tool for testing bandwidth and latency
-# Author: mahongxin <hongxin_228@163.com>
-
+# A#uthor: mahongxin <hongxin_228@163.com>
+#
 
 cd ../../../../utils
     . ./sys_info.sh
+  
+       "opensuse")            pkgs="mariadb expect java-1_8_0-openjdk"
+inst#all_deps "${pkgs}"
+prin#t_info $? installd-pkgs
+tar -xvf mycat.tar.gz -C /usr/local
+print_info $? tar-mycadt
+;; ./sh-test-lib
 cd -
-
-#set -x
-outDebugInfo
+|opensuse
+#set# -x
+outDe#bugInfo
 # Test user id
 if [ `whoami` != 'root' ] ; then
     echo "You must be the superuser to run this script" >&2
+#download source pakgs
     exit 1
 fi
 
 
-source ../percona/mysql.sh 
-
+source ../percona/mysql       wget 
+case $distro in     ub
+#start mysqluntu|debian|opensuse|fe       tar -xvf mesact.tar.gz -C /usr/local
 case $distro in
-    "centos")
-        cleanup_all_database
-        yum install java-1.8.0-openjdk.aarch64 -y
-        yum install mysql-community-server.aarch64 -y
-        yum install expect -y
-        yum install mycat -y
-        print_info $? install-package
-
+      "centos")
+         cleanup_all_database
+         pkgs="java-1.8.0-openjdk.aarch64 mysql-community-server.aarch64 expect mycat"
+         install_deps "${pkgs}"
+         print_info $? install-package
          ;;
+      "ubuntu")
+	  pkgs="openjdk-8-jdk mysql-server expect"
+	  install_deps "${pkgs}"
+	  print_info $? install-package
+	  tar -xvf mycat.tar.gz -C /usr/local
+	  print_info $? tar-mycat
+	  ;;http://192.168.50.122:8083/test_dependents/mycat.tar.gz
+      "fedora")
+          pkgs="community-mysql-server expect java-1.8.0-openjdk"
+          install_deps "${pkgs}"
+          print_info $? install-pkgs
+          tar -xvf mycat.tar.gz -C /usr/local
+          print_info $? tar-mycat
+          ;;
+#systemctl start mysql
+case $distro in
+   fedora)
+    systemctl start mysqld
+    print_info $? start-mysqld
+    systemctl status mysqld |grep "active (running)"
+    print_info $? mysqld-status
+    ;;
+   centos|ubuntu)
+     systemctl start mysql
+     print_info $? star-mysql
+     systemctl status mysql |grep "active (running)"
+     print_info $? mysql-stauts
+     ;;
 esac
-service mysqld start
-print_info $? start-mysqld
-systemctl status mysqld.service |grep "active (running)"
-print_info $? mysql-status
+#systemctl status mysql |grep "active (running)"
+#print_info $? mysql-status
 
 #修改密码
 mysqladmin -uroot  password "123456"
@@ -45,13 +77,13 @@ $EXPECT << EOF | tee -a out.log
 set timeout 100
 spawn mysql -uroot -p
 expect "Enter password"
-send "123456\n"
+send "root\n"
 expect "mysql>"
 send "create database db1;\n"
 expect "mysql>"
-send "create datebase db2;\n"
+send "create database db2;\n"
 expect "mysql>"
-send "create datebase db3;\n"
+send "create database db3;\n"
 expect "mysql>"
 send "exit;\n"
 expect eof
@@ -68,17 +100,21 @@ id mycat
 if [ $? -eq 0 ];then
     userdel mycat 
 fi 
-adduser -r -g mycat mycat
-print_info $? adduser-mycat
+#adduser  -g mycat mycat
+#print_info $? adduser-mycat
 #把mycat包放在/usr/local路径下面
-cd /usr/share/
-cp -r mycat ../../usr/local/
-print_info $? cp-usr-local
-cat /usr/local/mycat/version.txt |grep "MavenVersion"
-print_info $? mycat-version
-#修改mycat所属组
-chown -R mycat.mycat /usr/local/mycat
-print_info $? chown-mycat
+case $distro in
+    centos)
+      cd /usr/share/
+      cp -r mycat ../../usr/local/
+      print_info $? cp-usr-local
+      cat /usr/local/mycat/version.txt |grep "MavenVersion"
+      print_info $? mycat-version
+      #修改mycat所属组
+      chown -R mycat.mycat /usr/local/mycat
+      print_info $? chown-mycat
+      ;;
+esac
 #修改此项是为了解决mycat登录失败报错：java.lang.outofmemoryerror:direct buff
 #memory
 sed -i 's/wrapper.java.additional.5=-XX:MaxDirectMemorySize=2G/wrapper.java.additional.5=-XX:MaxDirectMemorySize=4G/g' /usr/local/mycat/conf/wrapper.conf
@@ -101,7 +137,7 @@ $EXPECT << EOF | tee -a mycat.log
 set timeout 100
 spawn mysql -uroot -p -h127.0.0.1 -P8066 -DTESTDB
 expect "Enter password"
-send "123456\n"
+send "root\n"
 expect "mysql>"
 send "create table employee (id int not null primary key,name varchar(100),sharding_id int not null);\n"
 expect "mysql>"
@@ -120,19 +156,19 @@ count=`grep "Query OK" mycat.log|wc -l`
 if [ "$count" -eq  4 ]; then
     print_info $? creat-table
     print_info $? insert-table
-    print_info $? explain-create
-    print_info $? explain-insert
+#    print_info $? explain-create
+ #  print_info $? explain-insert
 fi 
 
-systemctl stop mysql 
-./mycat stop 
+sy#stemctl stop mysql 
+#./mycat stop 
 print_info $? "mycat_stop"
 
-
+:
 cd - 
-yum remove expect -y
-yum remove java-1.8.0-openjdk.aarch64 -y
-yum remove mycat -y
-yum remove mysql-community-server.aarch64 -y
-print_info $? remove-package
-
+case $distro in
+    "centos"|"ubuntu"|"fedora"|"opensuse"|"debian")
+     #remove_deps "${pkgs}"
+     print_info $? remove_package
+     ;;
+esac
--- auto-test/middleware/tool/nicstat/nicstat.sh
+++ auto-test/middleware/tool/nicstat/nicstat.sh
@@ -1,17 +1,29 @@
 
 
-#!/bin/sh 
+#!/bin/bash
+### 2018-9-13 estuary Limited
+###Author:mahongxin
+###nicstat:网络流量统计工具
 set -x
+#########加载公共函数文件######
 cd ../../../../utils
-    .        ./sys_info.sh
-    .        ./sh-test-lib
+    source        ./sys_info.sh
+    source        ./sh-test-lib
 cd -
+##########判断当前用户是否是root#####
+! check_root && error_msg "Please run this script"
+###########nicstat选项#############
+### -s：显示摘要信息
+### -x: 显示扩展的输出
+### -M：以Mbps显示吞吐量，而不是默认的kB/s
+### -p: 以解析后的输出格式显示
+### -t: tcp流量统计
+### -u: ucp流量统计
+### -a: 等同于'-x -t -u'
+### -l: 只显示端口状态
+### -i：指定端口
 
-if [ `whoami` != 'root' ] ; then
-    echo "You must be the superuser to run this script" >&2
-    exit 1
-fi
-
+########################Environmental preparation #######
 version="1.95"
 from_repo="Estuary"
 pkgs="nicstat"
@@ -23,36 +35,51 @@ case $distro in
          ;;
  esac
 
-
-# Statistic ethernet flux 5 times
+##################testing tht stp#####################
+###每一秒显示一次显示5次
 nicstat 1 5
 print_info $? statistics
 
-# Statistic ethernet tcp flux
+###测试tcp
 nicstat -t 1 5
 print_info $? tcp
 
-# Statistic ethernet udp flux
+###测试udp 
 nicstat -u 1 5
 print_info $? udp
 
-# track interface
+###只显示正在使用的网口的流量 
 inet=`ip link|grep "state UP"|awk '{print $2}'|sed 's/://g'|awk '{print$1}'|head -1`
 nicstat -i $inet
 print_info $? network
 
-#output in Mbits/sec
+###以Mbits/sec为单位显示吞吐量
 nicstat -M
 print_info $? Mbits/sec
 
-#list interface(s)
+###显示端口状态
 nicstat -l
 print_info $? list_interface
 
-#summary output
+###显示摘要信息（只是接收和发送的数据量）
 nicstat -s
 print_info $? summary
-
-# Remove package
+##############举例说明#######
+#nicstat 1 5
+#Time          Int   rKB/s   wKB/s   rPk/s   wPk/s    rAvs    wAvs %Util    Sat
+#15:30:40  docker0    0.00    0.00    0.00    0.00    0.00    0.00  0.00   0.00
+#15:30:40       lo    0.00    0.00    0.00    0.00    0.00    0.00  0.00   0.00
+#15:30:40 enahisic2i0    0.23    1.34    3.00    6.00   79.67   228.7  0.00   0.00
+#Time列：表示当前采样的响应时间
+#lo and enahisisic2i0 : 网卡名称
+#rkB/s:每秒接收的千字节数
+#wKB/s:每秒写的千字节数
+#rPk/s：每秒接收的数据包数目
+#wPk/s:每秒写的数据包的数目
+#rAvs:接收数据包的平均大小
+#wAvs:传输的数据包平均大小
+#%Utils:网卡利用率（百分比）
+#Sat:网卡每秒的错误数
+######################## environment restore##############
 remove_deps "${pkgs}"
 print_info $? remove-pkgs
