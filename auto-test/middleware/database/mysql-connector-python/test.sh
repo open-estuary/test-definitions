ad2e953f9c4d0d81f089fb190d0ecacfc85e76af
dingyu1 <1136920311@qq.com>
2018-09-26 17:47:58 +0800

    mysql-connector-python

--- auto-test/middleware/database/mysql-connector-python/test.sh
+++ auto-test/middleware/database/mysql-connector-python/test.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 systemctl stop mysql
-packages=`apt list --installed | grep -i "mysql"|awk -F '/' '{print $1}'`
-for package in $packages
+packages1=`apt list --installed | grep -i "mysql"|awk -F '/' '{print $1}'`
+for package_a in $packages1
 do 
-	apt remove -y $package
+	apt remove -y $package_a
+done
+
+packages2=`apt list --installed | grep -i "mariadb"|awk -F '/' '{print $1}'`
+for package_b in $packages2
+do
+        apt remove -y $package_b
 done
 
