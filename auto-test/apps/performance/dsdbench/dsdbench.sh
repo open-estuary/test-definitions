b0488c6c3c1a62d229c1c7894f7834025f37083b
liubeijie <m15177445676>
2018-11-06 13:46:05 +0800

    dsdbench

--- auto-test/apps/performance/dsdbench/dsdbench.sh
+++ auto-test/apps/performance/dsdbench/dsdbench.sh
@@ -67,14 +67,14 @@ export GOPATH="${OUTPUT}/golang"
 #print_info $? down-dsdbench
 #cd dsdbench
 #cp目录
-case $distro in
-"centos")
+#case $distro in
+#"centos")
 wget http://htsat.vicp.cc:804/liubeijie/dsdbench.zip
 unzip dsdbench.zip
 print_info $? down-dsdbench
 cd dsdbench
-;;
-esac
+#;;
+#esac
 
 #cp目录
 cp -r vendor/ "${GOPATH}/src"
