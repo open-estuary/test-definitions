2a82894a4e33ebfabca9ad0c8e851e08a7b4c578
liubeijie <m15177445676>
2018-11-09 17:01:08 +0800

    debian_dns

--- auto-test/distributions/distribution/dns/debian_dns.sh
+++ auto-test/distributions/distribution/dns/debian_dns.sh
@@ -60,8 +60,8 @@ board_ip=`ifconfig |grep "inet"|cut -c14-26|head -n 1`
 sed -i "s/nameserver/# nameserver/g" /etc/resolv.conf
 sed -i "1i nameserver ${board_ip}" /etc/resolv.conf
 service bind9 restart
-#host www.tonv.my
-#sleep 5
-#print_info $? forward-dns
+host www.tonv.my
+sleep 5
+print_info $? forward-dns
 host 192.168.1.70 2>&1 | tee -a dig.log
 print_info $? reverse-dns
