1547347074b25427b436d829c371e8f79d94c444
fangyuanzheng <fyuanz_2010@yeah.net>
2018-11-22 14:40:15 +0800

    optimize

--- auto-test/kernel/linux/uname/uname.sh
+++ auto-test/kernel/linux/uname/uname.sh
@@ -1,22 +1,74 @@
-#!/bin/bash 
+# =================================================================
+#   Filename: uname
+#   Author:  
+#   Email: 
+#   Date: 2018-11-22 
+#   Description: Print certain system information.
+# ================================================================
+
+################## specify interpreter path ##################
+
+#!/bin/bash
+
+############# importing environment variable ##############
+
 cd ../../../../utils
-    .        ./sys_info.sh
-    .         ./sh-test-lib
+   .         ./sys_info.sh
+   .         ./sh-test-lib
 cd -
-if [ `whoami` != 'root' ] ; then
-    echo "You must be the superuser to run this script" >&2
-    exit 1
+
+#################### precheck ####################
+
+if [ 'whoami' != 'root' ];then
+	echo "You must be the root to run this script" >$2
+	exit 1
 fi
 
-version="4.18"
+################## setting variables ##################
 
-# Check the version 
+OUTPUT="$(pwd)/output"
+RESULT_FILE="${OUTPUT}/result.txt"
+export RESULT_FILE
+
+# pkgs="uname"
+version="4.18"
+machine="aarch64"
+
+#################### install ####################
+
+case $distro in
+    "centos")
+        install_deps "${pkgs}"
+        print_info $? install-pkgs
+        ;;
+    "debian")
+        install_deps "${pkgs}" -y
+        print_info $? install-pkgs
+        ;;
+esac
+
+#################### testing step ####################
+
+## Check the version ##
 vers=`uname -r|cut -b 1-4`
 echo $vers
 if [ "$vers" = "$version" ];then
-      print_info $? version
+      print_info $0 version
 else
-      print_info $? version
+      print_info $1 version
 fi
+
+## check the machine ##
+mach='uname -m'
+echo $mach
+if [ "$mach" = "$machine" ];then
+	print_info $0 machine
+else
+	print_info $1 machine
+fi
+
+################## restore environment ##################
+
+
 
 
