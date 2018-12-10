751d76b7319678158128e69c0f0a955f51cb90ae
dingyu1 <1136920311@qq.com>
2018-12-10 10:12:29 +0800

    modify qemu

--- auto-test/virtualization/virtual/qemu/qemu-start-kvm.sh
+++ auto-test/virtualization/virtual/qemu/qemu-start-kvm.sh
@@ -3,7 +3,7 @@
 set IMAGE [lindex $argv 0]
 set DISK [lindex $argv 1]
 
-spawn qemu-system-aarch64 -machine virt -cpu host -kernel $IMAGE -drive if=none,file=${DISK},id=fs -device virtio-blk-device,drive=fs -append "console=ttyAMA0 root=/dev/vda1" -nographic -D -d -enable-kvm
+spawn qemu-system-aarch64 -smp 1 -machine virt -cpu cortex-a57 -kernel $IMAGE -drive if=none,file=${DISK},id=fs -device virtio-blk-device,drive=fs --append "console=ttyAMA0 root=/dev/vda1" -nographic -D -d
 set timeout 20
 expect "estuary:/$"
 send "mount -t ext4 -o remount,rw /dev/vda1 /\n"
