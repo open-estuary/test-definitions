24cc79047f2bdea7fb5feb4f40fc8cae193a1014
mahongxin <mWX547872@china.huawei.com>
2019-01-15 18:18:23 +0800

    readFile.js

--- auto-test/middleware/language/nodejs/readFile.js
+++ auto-test/middleware/language/nodejs/readFile.js
@@ -92,13 +92,13 @@ function openFile(){
     var fs = require("fs");
     var exec = require('child_process').exec;
     fs.writeFile('tmp.txt',"this is also a temp file\n");
-    fs.open('tmp.txt', 'r+', function(err, fd) {
+    /*fs.open('tmp.txt', 'r+', function(err, fd) {
         if (err) {
             exec('lava-test-case "nodejs_open_file_async " --result fail' , function(err , stdout ,stderr ){console.log(stdout) ;} );
             return console.error(err);
 
         }
-        exec('lava-test-case "nodejs_open_file_async " --result pass' , function(err , stdout ,stderr ){console.log(stdout) ;} );
+        exec('lava-test-case "nodejs_open_file_async " --result pass' , function(err , stdout ,stderr ){console.log(stdout) ;} );*/
 
         fs.close(fd, function(err){
             if (err){
