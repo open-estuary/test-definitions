3cadaa001895a5be42da05bc12d8e1cb2b071d20
dingyu1 <1136920311@qq.com>
2019-01-22 11:47:21 +0800

    modify

--- auto-test/middleware/database/mongo/mongodb-test.sh
+++ auto-test/middleware/database/mongo/mongodb-test.sh
@@ -14,14 +14,14 @@ source ./mongodb.sh
 #export PS4='+$LINENO:$FUNCTION[0] '
 outDebugInfo
 install_mongodb
-mongodb_start
 
+mongodb_start_by_service
+mongodb_stop_by_service
 
 mongodb_client
 mongodb_shutdown
 
-mongodb_start_by_service
-mongodb_stop_by_service
+
 
 mongodb_uninstall
 
