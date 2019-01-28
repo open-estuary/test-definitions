2677eb04f42f00a395ed6a596b59fbce860f546a
flytable <35858303+flytable@users.noreply.github.com>
2019-01-28 15:15:54 +0800

    Update docker.sh

--- auto-test/virtualization/virtual/docker/docker.sh
+++ auto-test/virtualization/virtual/docker/docker.sh
@@ -167,6 +167,11 @@ rm -rf upload
 #rm -rf rm -rf /var/lib/docker
 rm -rf /var/lib/docker
 
+if [ "${ci_http_addr}"x = "http://172.19.20.15:8083"x ];then
+    rm -rf ~/Discuz           #需要删除
+    rm -rf ~/mysql_data       #需要删除
+fi
+
 case "${distro}" in
     centos|fedora|opensuse)
         pkgs_re="docker"
@@ -180,6 +185,12 @@ case "${distro}" in
 	;;
     debian)
 	rm -rf get-docker.sh
+	
+	if [ "${ci_http_addr}"x = "http://172.19.20.15:8083"x ];then
+	    service docker stop       #需要停止
+	    apt-get remove docker*
+	fi
+	
 	apt-get purge docker-ce -y
         print_info $? remove_docker
 	;;
