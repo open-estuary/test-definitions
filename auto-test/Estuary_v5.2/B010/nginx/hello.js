5cfed8b5f36253fce6bd549842a26c92ef607c83
liubeijie <m151774456>
2019-07-02 10:01:33 +0800

    e

--- auto-test/Estuary_v5.2/B010/nginx/hello.js
+++
@@ -1,4 +0,0 @@
-function hello(r) {
-
-       r.return(200,"hello world~")
-}
--- auto-test/Estuary_v5.2/B010/qemu/debian_qcow2.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/expect
-
-set timeout 150
-spawn scp zhangwangqun@192.168.1.107:/home/zhangwangqun/debian.qcow2 .
-expect {
-")?" {send "yes\r";exp_continue}
-"password:" {send "zhang12\r"}
-}
-expect eof
